a9142b7ea6ad2afa5d57574e2c88c471ef9cf944
David Benoit <david.j.benoit1@gmail.com>
2022-02-09 06:05:40 -0600

    Finish and refactor controller

--- server/controllers/movie-ctrl.js
+++ server/controllers/movie-ctrl.js
@@ -8,32 +8,36 @@ createMovie = (req, res) => {
   const body = req.body;
 
   if (!body) {
-    return res.status(400).json({
-      success: false,
-      error: 'You must provide a movie.',
-    })
+    return res
+      .status(400)
+      .json({ success: false, error: 'You must provide a movie.', })
   }
 
   const movie = new Movie(body);
 
   if (!movie) {
-    return res.status(400).json({ success: false, error: err })
+    return res
+      .status(400)
+      .json({ success: false, error: err })
   }
 
   movie
     .save()
     .then(() => {
-      return res.status(201).json({
-        success: true,
-        id: movie.id,
-        message: 'Movie created!',
-      })
+      return res
+        .status(201)
+        .json({
+          success: true,
+          id: movie.id,
+          message: 'Movie created!',
+        })
     })
     .catch(error => {
-      return res.status(400).json({
-        error,
-        message: 'Movie not created.',
-      })
+      return res
+        .status(400)
+        .json({
+          error, message: 'Movie not created.',
+        })
     })
 }
 //update
@@ -41,22 +45,22 @@ updateMovie = async (req, res) => {
   const body = req.body;
 
   if (!body) {
-    return res.status(400).json({
-      success: false,
-      error: 'You must provide a body to update.',
-    })
+    return res
+      .status(400)
+      .json({ success: false, error: 'You must provide a body to update.', })
   }
 
   Movie.findOne({ _id: req.params.id }, (err, movie) => {
     if (err) {
-      return res.status(404).json({
-        err,
-        message: 'Movie not found!',
-      })
+      return res
+        .status(404)
+        .json({ err, message: 'Movie not found!', })
     }
+
     movie.name = body.name;
     movie.time = body.time;
     movie.rating = body.rating;
+
     movie
       .save()
       .then(() => {
@@ -67,10 +71,9 @@ updateMovie = async (req, res) => {
         })
       })
       .catch(error => {
-        return res.status(404).json({
-          error,
-          message: 'Movie not updated!',
-        })
+        return res
+          .status(404)
+          .json({ error, message: 'Movie not updated!', })
       })
   })
 }
@@ -79,16 +82,71 @@ updateMovie = async (req, res) => {
 deleteMovie = async (req, res) => {
   await Movie.findOneAndDelete({ _id: req.params.id }, (err, movie) => {
     if (err) {
-      return res.status(400).json({ success: false, error: err })
+      return res
+        .status(400)
+        .json({ success: false, error: err })
     }
 
     if (!movie) {
-      return res.status(404).json({ success: false, error: "Movie not found" })
+      return res
+        .status(404)
+        .json({ success: false, error: "Movie not found" })
     }
 
-    return res.status(200).json({ success: true, data: movie })
-  }).catch(err => console.log(err))
+    return res
+      .status(200)
+      .json({ success: true, data: movie })
+
+  }).catch(err => console.log(err)) //if any error is caught
 }
+
 //return one movie by ID
+getMovieById = async (req, res) => {
+  await Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res
+        .status(400)
+        .json({ success: false, error: err })
+    }
+
+    if (!movie) {
+      return res
+        .status(404)
+        .json({ success: false, error: 'Movie not found.' })
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, data: movie })
+
+  }).catch(err => console.log(err)) //if any error is caught
+}
 
 //return all movies
+getMovies = async (req, res) => {
+  await Movie.find({}, (err, movies) => {
+    if (err) {
+      return res
+        .status(400)
+        .json({ success: false, error: err })
+    }
+
+    if (!movies.length) {
+      return res
+        .status(404)
+        .json({ success: false, error: 'Movie not found.' })
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, data: movies })
+  }).catch(err => console.log(err))
+}
+
+module.exports = {
+  createMovie,
+  updateMovie,
+  deleteMovie,
+  getMovieById,
+  getMovies,
+}
